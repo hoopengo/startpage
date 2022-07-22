127986d86a0a732965080252e2228c46168ae53e
hoopengo <hoopengo@yandex.ru>
2022-07-22 17:22:51 +0300

    add google search [ctrl+/]

--- js/index.js
+++ js/index.js
@@ -7,10 +7,46 @@ function update_time() {
     solo = (new Date()).toLocaleTimeString(["ru-RU"], {hour: '2-digit', minute:'2-digit', second: '2-digit'})
     mono = (new Date()).toLocaleDateString(["id"]);
 
-    date = `${weekday}, ${solo} | ${mono}`
+    date = `${weekday}, ${solo}, ${mono}`
 
     document.getElementById("updatetime").innerHTML = date;
     setTimeout(update_time, 1000);
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 update_time()
+
+// Search
+
+search_input = document.getElementById("textbox")
+
+search_input.addEventListener('blur', async function(event) {
+    search = document.getElementById("show-on-slash")
+
+    for (let i = 1; i > 0; i = i - 0.1) {
+        search.style.opacity = i;
+        await sleep(10)
+    }
+})
+
+search_input.addEventListener('focus', async function(event) {
+    search = document.getElementById("show-on-slash")
+
+    for (let i = 0; i < 1; i = i + 0.1) {
+        search.style.opacity = i;
+        await sleep(10)
+    }
+})
+
+document.addEventListener('keydown', async function(event) {
+    if (event.code == 'Slash' && (event.ctrlKey || event.metaKey)) {
+        search = document.getElementById("show-on-slash")
+        search.style.display = 'block';
+        search_input.focus();
+
+        location = 'https://google.com/'
+    }
+});
